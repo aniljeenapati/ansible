a0bdab8d87c18203002f19c8d33e10df1f63cf13
aniljeenapati <jeenapati.anil@gmail.com>
2024-10-25 18:02:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,19 +33,16 @@ output "vm_ip" {
 resource "null_resource" "update_inventory" {
   provisioner "local-exec" {
     command = <<EOT
-      INSTANCE_IDS=$(gcloud compute instance-groups list-instances centos-instance-group --zone us-central1-a --format="value(instance)")
+      INSTANCE_ID=${google_compute_instance.centos_vm.name}
+      INSTANCE_IP=${google_compute_instance.centos_vm.network_interface.0.access_config.0.nat_ip}
       echo 'all:' > /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
       echo '  hosts:' >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
-      for INSTANCE_ID in $INSTANCE_IDS; do
-        INSTANCE_IP=$(gcloud compute instances describe $INSTANCE_ID --zone us-central1-a --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
-        echo "    web_$INSTANCE_ID:" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
-        echo "      ansible_host: $INSTANCE_IP" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
-        echo "      ansible_user: centos" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
-        echo "      ansible_ssh_private_key_file: /root/.ssh/id_rsa" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
-      done
+      echo "    web_$INSTANCE_ID:" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
+      echo "      ansible_host: $INSTANCE_IP" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
+      echo "      ansible_user: centos" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
+      echo "      ansible_ssh_private_key_file: /root/.ssh/id_rsa" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
     EOT
   }
 
-  depends_on = [google_compute_instance_group_manager.instance_group]
+  depends_on = [google_compute_instance.centos_vm]
 }
-
